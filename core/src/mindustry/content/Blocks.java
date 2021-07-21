f286abe6c94627c9273f065e281a3aa877defbdb
Ksawi <66205430+Ksawi999@users.noreply.github.com>
2021-07-21 21:30:35 +0200

    As in master branch

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -238,7 +238,6 @@ public class Blocks implements ContentList{
             attributes.set(Attribute.heat, 0.75f);
             attributes.set(Attribute.surfaceHeat, 0.75f);
             attributes.set(Attribute.water, -0.75f);
-            updateEffect = Fx.magmasmoke;
             blendGroup = basalt;
 
             emitLight = true;
